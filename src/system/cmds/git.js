5c28681464569d6c933a2a20d2c3030c5e65e947
= <=>
2024-02-02 09:52:00 -0600

    Add git

--- src/system/cmds/git.js
+++ src/system/cmds/git.js
@@ -12,15 +12,36 @@ export class GitCommand extends Command {
         );
     }
 
-    async execute(args, kwargs, connection) { 
-
-        for (let i = 0; i < 10; i++) {
-            await new Promise(resolve => setTimeout(resolve, 100));
-            await connection.stdOut(`(+)`);
+    async loadingBar(connection, numLines) {
+        const randomI = Math.floor(Math.random() * numLines);
+        const randomJ = Math.floor(Math.random() * 50);
+        for (let i = 0; i < numLines; i++) {
+            await connection.stdOut("[");
+            let speed = Math.floor(Math.random() * 10) + 1; // Random speed between 1 and 10
+            for (let j = 0; j < 50; j++) {
+                if (i === randomI && j === randomJ) {
+                    speed = 1;
+                }
+                await new Promise(resolve => setTimeout(resolve, i === randomI && j === randomJ ? 500 : speed));
+                await connection.stdOut(`#`);
+                speed += Math.floor(Math.random() * 3) - 1; // Randomly increase or decrease speed by 1
+                speed = Math.max(1, speed); // Ensure speed is at least 1
+            }
+            await connection.stdOut("]\n\n");
         }
-        
+    }
+
+
+    async execute(args, kwargs, connection) { 
+        await connection.stdOut("Doing some 'git' stuff...\n\n");
+        await this.loadingBar(connection, 5);
+        await connection.stdOut("Done!\n\n");
+        await new Promise(resolve => setTimeout(resolve, 1000));
+        await connection.stdOut("Just kidding, nobody implemented it yet.\n\n");
+        await this.loadingBar(connection, 1);
+        await connection.stdOut(":)\n\n")
         return new Result(
-            "git: test",
+            "",
             ResultStatus.IMPROPER_COMMAND
         );
     }
